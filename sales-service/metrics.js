3ba089efe68f10f9c22b0a14b4e7724072fbea57
Isaac Noriega <isaac.noriega@iteso.mx>
2025-11-24 19:54:30 -0600

    ya casi

--- sales-service/metrics.js
+++ sales-service/metrics.js
@@ -1,40 +1,37 @@
 const AWS = require('aws-sdk');
 
-// Configuración regional
 const cloudwatch = new AWS.CloudWatch({ region: process.env.AWS_REGION || 'us-east-1' });
+
 const ENV = process.env.NODE_ENV || "LOCAL";
 
 // Helper para enviar métricas
 const logMetric = async (metricName, value, unit, dimensions = {}) => {
     
-    //Log Local 
     console.log(`[METRICA - ${ENV}] ${metricName}: ${value} ${JSON.stringify(dimensions)}`);
 
-    // 2. Envio a Nube 
-    if (ENV === "PRODUCTION") {
-        try {
-            // Convertimos el objeto dimensions a formato AWS
-            const awsDimensions = [
-                { Name: 'Environment', Value: 'PROD' }, // Dimensión Fija
-                ...Object.keys(dimensions).map(key => ({ Name: key, Value: dimensions[key] }))
-            ];
+    try {
 
-            const params = {
-                MetricData: [
-                    {
-                        MetricName: metricName,
-                        Dimensions: awsDimensions,
-                        Unit: unit,
-                        Value: value
-                    },
-                ],
-                Namespace: 'ExamenFinal/App' 
-            };
+        const awsDimensions = [
+            { Name: 'Environment', Value: ENV }, 
+            ...Object.keys(dimensions).map(key => ({ Name: key, Value: dimensions[key] }))
+        ];
 
-            await cloudwatch.putMetricData(params).promise();
-        } catch (error) {
-            console.error("Error métrica:", error.message);
-        }
+        const params = {
+            MetricData: [
+                {
+                    MetricName: metricName,
+                    Dimensions: awsDimensions,
+                    Unit: unit,
+                    Value: value
+                },
+            ],
+            Namespace: 'ExamenFinal/App' 
+        };
+
+        await cloudwatch.putMetricData(params).promise();
+        
+    } catch (error) {
+        console.error("No se pudo enviar métrica a AWS:", error.message);
     }
 };
 
